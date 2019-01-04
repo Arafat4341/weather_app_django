bfa7236dc7cadc5e7f8632a736755f4c001b9b86
Arafat4341 <mdyasinarafatyen2@gmail.com>
2019-01-03 17:22:37 -0800

    fetched the weather data from the api

--- weather/views.py
+++ weather/views.py
@@ -1,4 +1,14 @@
+import requests
 from django.shortcuts import render
 
 def index(request):
+	url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&APPID=45e86a0a63d11da27b8a03dc4e474f69'
+	city = 'Dhaka'
+	r = requests.get(url.format(city)).json()
+	city_weather = {
+		'city': city,
+		'temperature': r['main']['temp'],
+		'description': r['weather'][0]['description'],
+		'icon': r['weather'][0]['icon']
+	}
 	return render(request, 'weather/weather.html')
